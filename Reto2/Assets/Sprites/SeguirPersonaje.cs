ce165996cf284913821f2a57792ad837d73cc660
Camilo <camilogomezr25@gmail.com>
2023-07-27 21:02:36 -0500

    Creacion base dos niveles

---
+++ Reto2/Assets/Sprites/SeguirPersonaje.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeguirPersonaje : MonoBehaviour
+{
+    public Transform jugador;
+    public float distanciaX = 1f;
+    public float distanciaY= 1f;
+
+    void Update()
+    {
+        transform.position = new Vector3(jugador.position.x + distanciaX, jugador.position.y+distanciaY, transform.position.z); 
+        
+    }
+}
